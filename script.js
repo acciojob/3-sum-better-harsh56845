9b40f96402770b9cd3d6aaf12ddd4de21521de03
harsh56845 <56841891+harsh56845@users.noreply.github.com>
2023-05-24 00:12:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-function threeSum(arr, target) {
+function threeSum(S, target) {
 // write your code here
+	 S.sort((a, b) => a - b);
+
+  // Initialize the minimum difference to infinity.
+  let minDiff = Infinity;
+  let minSum = 0;
+
+  // Iterate over all pairs of elements in the array.
+  for (let i = 0; i < S.length - 2; i++) {
+    for (let j = i + 1; j < S.length - 1; j++) {
+      // Find the third element that makes the sum closest to the target.
+      let k = S.indexOf(target - S[i] - S[j]);
+
+      // If the third element is found, update the minimum difference and minimum sum.
+      if (k >= 0) {
+        let diff = Math.abs(target - S[i] - S[j] - S[k]);
+        if (diff < minDiff) {
+          minDiff = diff;
+          minSum = S[i] + S[j] + S[k];
+        }
+      }
+    }
+  }
+
+  // Return the minimum sum.
+  return minSum;
   
 }
 
